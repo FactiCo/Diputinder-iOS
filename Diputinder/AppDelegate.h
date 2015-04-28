fe538bd821e19db1a6e75c6377c7b40348c77f12
Carlos Castellanos <rockarlos@me.com>
2015-04-28 16:30:21 -0500

    Initial Commit

---
+++ Diputinder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Diputinder
+//
+//  Created by Carlos Castellanos on 28/04/15.
+//  Copyright (c) 2015 Carlos Castellanos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
