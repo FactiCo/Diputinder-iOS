7df7868c251daddd23f70b4bf0c32ed9cdf04e4f
Carlos Castellanos <rockarlos@me.com>
2015-04-29 14:45:17 -0500

    agregue que identifique en que estado te encuentras

--- Diputinder/AppDelegate.h
+++ Diputinder/AppDelegate.h
@@ -7,9 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate,CLLocationManagerDelegate>
+{
 
+    CLLocationManager *locationManager;
+}
+@property (strong, nonatomic) NSString *localidad;
 @property (strong, nonatomic) UIWindow *window;
 
 
--- Diputinder/AppDelegate.m
+++ Diputinder/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import <AFHTTPRequestOperationManager.h>
+#define IS_OS_8_OR_LATER ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
 
 @interface AppDelegate ()
 
@@ -17,9 +19,80 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    // Permisos para localizar al usuario
+    locationManager = [[CLLocationManager alloc]init]; // initializing locationManager
+    locationManager.delegate = self; // we set the delegate of locationManager to self.
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest; // setting the accuracy
+    
+    [locationManager startUpdatingLocation];
+    
+#ifdef __IPHONE_8_0
+    if(IS_OS_8_OR_LATER) {
+        // Use one or the other, not both. Depending on what you put in info.plist
+        [locationManager requestWhenInUseAuthorization];
+        [locationManager requestAlwaysAuthorization];
+    }
+#endif
+    
+    [self getAddress];
+  //  [self reverseGeokcode:locationManager.location];
     return YES;
 }
+-(void)getAddress{
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    
+    
+    NSString *url =[NSString stringWithFormat:@"http://maps.googleapis.com/maps/api/geocode/json?sensor=true&latlng=%f,%f",locationManager.location.coordinate.latitude,locationManager.location.coordinate.longitude];
+    
+    [manager GET:url parameters:@{} success:^(AFHTTPRequestOperation *operation, id responseObject){
+        
+        for (NSDictionary *item in [responseObject objectForKey:@"results"]) {
+           // administrative_area_level_1
+            NSArray *aux=[item objectForKey:@"address_components"];
+            for (int i=0; i<[aux count]; i++) {
+                NSArray *leves=[[aux objectAtIndex:i]objectForKey:@"types"];
+                for (NSString *a in leves) {
+             //   NSLog(@"%@",a);
+                    if ([a isEqualToString:@"administrative_area_level_1"]) {
+                          NSLog(@"%@",[[aux objectAtIndex:i]objectForKey:@"long_name"]);
+                        _localidad=[[aux objectAtIndex:i]objectForKey:@"long_name"];
+                    }
+                }
+        
+                
+            }
+         
+            break;
+        }
+       // if ([candidatos count]) {
+        //   }
+      //  else{
+            // No Success
+            //   NSLog(@"no hay ");
+        //}
+        
+        
+    }failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Error %@", error);
+        
+        
+    }];
 
+}
+/*- (void)reverseGeocode:(CLLocation *)location {
+    CLGeocoder *geocoder = [[CLGeocoder alloc] init];
+    [geocoder reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error) {
+        NSLog(@"Finding address");
+        if (error) {
+            NSLog(@"Error %@", error.description);
+        } else {
+            CLPlacemark *placemark = [placemarks lastObject];
+            NSString *address = [NSString stringWithFormat:@"%d", ABCreateStringWithAddressDictionary(placemark.addressDictionary, NO)];
+            NSLog(@"%@",address);
+        }
+    }];
+}*/
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
